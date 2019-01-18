f4d677259b1200ed9a935f4cfb0b2584152b8f2f
Brian Luu <luubri5@uw.edu>
2019-01-17 16:02:54 -0800

    Tests passed

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,74 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func add(_ array: [Int]) -> Int {
+        var sum = 0;
+        for i in 0...array.count - 1 {
+            sum += array[i]
+        }
+        return sum
+    }
+    
+    func multiply(_ array: [Int]) -> Int {
+        var sum = 1;
+        for i in 0...array.count - 1 {
+            sum *= array[i]
+        }
+        return sum
+    }
+   
+    
+    func count(_ array: [Int]) -> Int {
+        return array.count
+    }
+  
+    func avg(_ array: [Int]) -> Int {
+       return add(array) / count(array)
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var final = beg
+        for i in args {
+            final = op(i, final)
+        }
+        return final
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": lhs["x"]! + rhs["x"]!, "y": lhs["y"]! + rhs["y"]!]
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": lhs["x"]! - rhs["x"]!, "y": lhs["y"]! - rhs["y"]!]
+    }
 }
+
+
--- ComplexCalc/ComplexCalc/main.swift
+++ ComplexCalc/ComplexCalc/main.swift
@@ -33,7 +33,7 @@ assert(calc.avg([1, 2, 3, 4, 5]) == 3)
 assert(calc.avg([1]) == 1)
 
 assert(calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6)
-    // this is (((0 op 1) op 2) op 3)
+    // this  (((0 op 1) op 2) op 3)
 assert(calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15)
     // this is (((((0 op 1) op 2) op 3) op 4) op 5)
 assert(calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1)
